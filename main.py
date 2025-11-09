7d4b3fc538c4c0e7561aa1df55d9522d39d14184
Kwon Yuho <103117239+museekee@users.noreply.github.com>
2025-11-09 16:33:01 +0900

    memorize 만들래 ㅠ

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 try:
-    from PySide6.QtWidgets import QApplication, QMainWindow
-    from PySide6.QtUiTools import QUiLoader
     from PySide6.QtCore import QFile
-    from pathlib import Path
+    from PySide6.QtWidgets import QApplication, QMainWindow, QDialog
+    from PySide6.QtUiTools import QUiLoader
 except ImportError:
-    # import importlib
     import pip
     import os
 
     print("PySide6 is not installed. Installing...")
     pip.main(["install", "PySide6"])
-    # QApplication = importlib.import_module("PySide6.QtWidgets").QApplication
-    # QUiLoader = importlib.import_module("PySide6.QtUiTools").QUiLoader
-    # QFile = importlib.import_module("PySide6.QtCore").QFile
+
     if os.system(f"python {__file__}") == 0:
         exit(0)
     elif os.system(f"py {__file__}") == 0:
@@ -22,6 +18,18 @@ except ImportError:
     else:
         print("Failed to start application. Please run it manually.")
         exit(1)
+
+
+class MemorizeDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        loader = QUiLoader()
+        self.ui = loader.load("./ui/memorize.ui", self)
+
+        self.setLayout(self.ui.layout())
+        self.setWindowTitle(self.ui.windowTitle())
+        self.resize(self.ui.size())
+        self.setFont(self.ui.font())
 
 
 class PortalWindow(QMainWindow):
@@ -34,6 +42,12 @@ class PortalWindow(QMainWindow):
         self.setWindowTitle(self.ui.windowTitle())
         self.setStyleSheet(self.ui.styleSheet())
         self.setFont(self.ui.font())
+
+        self.ui.btnMemorize.clicked.connect(self.on_btnMemorize_clicked)
+
+    def on_btnMemorize_clicked(self):
+        dialog = MemorizeDialog(self)
+        dialog.exec()
 
 
 if __name__ == "__main__":
