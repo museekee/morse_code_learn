543138aadcc03153896016830cf4ab0224ef7ae4
Kwon Yuho <103117239+museekee@users.noreply.github.com>
2025-11-09 10:35:49 +0900

    모듈 설치하면 재시동 하도록

--- main.py
+++ main.py
@@ -3,13 +3,24 @@ try:
     from PySide6.QtUiTools import QUiLoader
     from PySide6.QtCore import QFile
 except ImportError:
-    import importlib
-
+    # import importlib
     import pip
+    import os
 
     print("PySide6 is not installed. Installing...")
     pip.main(["install", "PySide6"])
-    PySide6 = importlib.import_module("PySide6")
+    # QApplication = importlib.import_module("PySide6.QtWidgets").QApplication
+    # QUiLoader = importlib.import_module("PySide6.QtUiTools").QUiLoader
+    # QFile = importlib.import_module("PySide6.QtCore").QFile
+    if os.system(f"python {__file__}") == 0:
+        exit(0)
+    elif os.system(f"py {__file__}") == 0:
+        exit(0)
+    elif os.system(f"python3 {__file__}") == 0:
+        exit(0)
+    else:
+        print("Failed to start application. Please run it manually.")
+        exit(1)
 
 app = QApplication([])
 loader = QUiLoader()
