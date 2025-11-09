84b6d60539e474ff2f1756ce9a684f82dc527ce3
Kwon Yuho <103117239+museekee@users.noreply.github.com>
2025-11-09 10:35:05 +0900

    class화

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 try:
-    from PySide6.QtWidgets import QApplication
+    from PySide6.QtWidgets import QApplication, QMainWindow
     from PySide6.QtUiTools import QUiLoader
     from PySide6.QtCore import QFile
+    from pathlib import Path
 except ImportError:
     import importlib
 
@@ -11,10 +12,21 @@ except ImportError:
     pip.main(["install", "PySide6"])
     PySide6 = importlib.import_module("PySide6")
 
-app = QApplication([])
-loader = QUiLoader()
-window = loader.load("./ui/portal.ui")
+
+class PortalWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        loader = QUiLoader()
+        self.ui = loader.load("./ui/portal.ui")
+        self.setCentralWidget(self.ui.centralWidget())
+        self.setGeometry(self.ui.geometry())
+        self.setWindowTitle(self.ui.windowTitle())
+        self.setStyleSheet(self.ui.styleSheet())
+        self.setFont(self.ui.font())
+
 
 if __name__ == "__main__":
+    app = QApplication([])
+    window = PortalWindow()
     window.show()
     app.exec()
