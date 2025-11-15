b6a178e5a5e5c7ccd1d6fca525aec503995cd582
Kwon Yuho <103117239+museekee@users.noreply.github.com>
2025-11-15 17:46:03 +0900

    learn창 만듦

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 try:
+    import os
     from PySide6.QtCore import QFile
-    from PySide6.QtWidgets import QApplication, QMainWindow, QDialog, QLabel
+    from PySide6.QtWidgets import QApplication, QMainWindow, QDialog, QLabel, QWidget
     from PySide6.QtUiTools import QUiLoader
-    from PySide6.QtGui import QPixmap, QPainter
+    from PySide6.QtGui import QPixmap, QPainter, QFont, QFontDatabase
     from PySide6.QtCore import Qt
     from PySide6.QtSvg import QSvgRenderer
     import darkdetect
@@ -23,10 +24,12 @@ except ImportError:
         print("Failed to start application. Please run it manually.")
         exit(1)
 
+
 def getPixmap(image_name: str) -> QPixmap:
     theme = "dark" if darkdetect.isDark() else "light"
     image_path = f"./assets/img/{theme}/{image_name}"
     return QPixmap(image_path)
+
 
 def getPixmapedSvg(image_name: str, width: int, height: int) -> QPixmap:
     theme = "dark" if darkdetect.isDark() else "light"
@@ -42,6 +45,7 @@ def getPixmapedSvg(image_name: str, width: int, height: int) -> QPixmap:
 
     return pixmap
 
+
 class MemorizeDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -53,14 +57,37 @@ class MemorizeDialog(QDialog):
         self.resize(self.ui.size())
         self.setFont(self.ui.font())
 
-        self.ui.NumberImage.setPixmap(getPixmapedSvg("숫자기호.svg", self.width()*3, self.height()*3).scaled(self.width(), self.height(), Qt.KeepAspectRatio))
-        self.ui.EngImage.setPixmap(getPixmapedSvg("알파벳.svg", self.width()*3, self.height()*3).scaled(self.width(), self.height(), Qt.KeepAspectRatio))
-        self.ui.HangulImage.setPixmap(getPixmapedSvg("한글.svg", self.width()*3, self.height()*3).scaled(self.width(), self.height(), Qt.KeepAspectRatio))
-        
+        self.ui.NumberImage.setPixmap(
+            getPixmapedSvg("숫자기호.svg", self.width()*3, self.height()*3)
+            .scaled(self.width(), self.height(), Qt.KeepAspectRatio)
+        )
+        self.ui.EngImage.setPixmap(
+            getPixmapedSvg("알파벳.svg", self.width()*3, self.height()*3)
+            .scaled(self.width(), self.height(), Qt.KeepAspectRatio)
+        )
+        self.ui.HangulImage.setPixmap(
+            getPixmapedSvg("한글.svg", self.width()*3, self.height()*3)
+            .scaled(self.width(), self.height(), Qt.KeepAspectRatio)
+        )
+
         self.ui.buttonBox.accepted.connect(self.on_ok_clicked)
 
     def on_ok_clicked(self):
         self.accept()
+
+
+class LearnDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        loader = QUiLoader()
+        self.ui = loader.load("./assets/learn.ui", self)
+
+        self.setGeometry(self.ui.geometry())
+        self.setWindowTitle(self.ui.windowTitle())
+        self.setStyleSheet(self.ui.styleSheet())
+        self.setFont(self.ui.font())
+        self.setFixedSize(self.ui.size())
+
 
 class PortalWindow(QMainWindow):
     def __init__(self):
@@ -72,16 +99,30 @@ class PortalWindow(QMainWindow):
         self.setWindowTitle(self.ui.windowTitle())
         self.setStyleSheet(self.ui.styleSheet())
         self.setFont(self.ui.font())
+        # jersey_font = QFont("Jersey 25", 48)
+        # self.ui.label.setFont(jersey_font)
 
         self.ui.btnMemorize.clicked.connect(self.on_btnMemorize_clicked)
+        self.ui.btnLearn.clicked.connect(self.on_btnLearn_clicked)
 
     def on_btnMemorize_clicked(self):
         dialog = MemorizeDialog(self)
         dialog.show()
 
+    def on_btnLearn_clicked(self):
+        learn_widget = LearnDialog(self)
+        learn_widget.exec()
+
 
 if __name__ == "__main__":
     app = QApplication([])
+
+    font_path = os.path.join(os.path.dirname(
+        __file__), "assets", "font", "Jersey25-Regular.ttf")
+    id = QFontDatabase.addApplicationFont(font_path)
+    jersey = QFontDatabase.applicationFontFamilies(id)[0]
+    # app.setFont(QFont(jersey, 10))
+
     window = PortalWindow()
     window.show()
     app.exec()
